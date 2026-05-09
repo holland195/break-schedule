98a8cb0a3302f67956521eb727fac2c46af022c3
holland195 <83504565+holland195@users.noreply.github.com>
2026-05-09 15:39:44 +0700

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDpkSsDS2HMvDl8EXoD5J23VXLyligTkFk",
+  authDomain: "break-schedule-pave.firebaseapp.com",
+  databaseURL: "https://break-schedule-pave-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "break-schedule-pave",
+  storageBucket: "break-schedule-pave.firebasestorage.app",
+  messagingSenderId: "1080497083744",
+  appId: "1:1080497083744:web:72915f7d298d039bfe3a05",
+  measurementId: "G-LQYK6LRZXP"
+};
